635d75ad1adbf767e486c2240240f7f9b95055cb
mz2 <matias.piipari@gmail.com>
2016-02-24 14:18:14 +0000

    Initial commit

---
+++ Sparkler/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Sparkler
+//
+//  Created by Matias Piipari on 24/02/2016.
+//  Copyright © 2016 Manuscripts.app Limited. All rights reserved.
+//
+
+import Foundation
+import Sparkling
+
+
---
+++ Sparkling/Sparkling.h
@@ -0,0 +1,19 @@
+//
+//  Sparkling.h
+//  Sparkling
+//
+//  Created by Matias Piipari on 24/02/2016.
+//  Copyright © 2016 Manuscripts.app Limited. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Sparkling.
+FOUNDATION_EXPORT double SparklingVersionNumber;
+
+//! Project version string for Sparkling.
+FOUNDATION_EXPORT const unsigned char SparklingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sparkling/PublicHeader.h>
+
+
