e04d0f9baf21a7a2491a0f3545892c273a09dbbd
mz2 <matias.piipari@gmail.com>
2016-02-24 18:48:01 +0000

    First functioning commandling parsing.

--- Sparkler/main.swift
+++ Sparkler/main.swift
@@ -9,6 +9,18 @@
 //
 
 import Foundation
-import Sparkling
+import SparklerKit
+import Commander
 
-print("foobar")
+Group {
+    
+    $0.command("download",
+        Argument<String>("appcast", description:"Path to the appcast whose entries we are to create deltas for."),
+        Option("number", 10, description:"Number or appcast items to build a delta for."),
+        Option("dsa", "", description:"Path to DSA private key for the updates."),
+        Option("working-directory", "", description:"Path to a working directory where updates downloaded from the appcast are stored."))
+        { appcast, number, dsa, workingDir in
+            
+        
+    }
+}.run()
